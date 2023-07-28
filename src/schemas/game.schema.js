3bbc4a1384f7d86a54853f9cd27d52c50bbc28a8
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-28 11:17:24 -0300

    Fix: game schema

--- src/schemas/game.schema.js
+++ src/schemas/game.schema.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-export const gameSchema = joi.object({
+export const gameSchema = Joi.object({
     name: joi.string().min(1).required(),
     image: joi.required(),
     stockTotal: joi.number().min(1).required(),
