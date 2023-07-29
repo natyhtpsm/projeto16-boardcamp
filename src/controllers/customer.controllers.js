11c873c961c217e05a58bf13660a9199625bd244
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-29 19:02:45 -0300

    Update: put customers

--- src/controllers/customer.controllers.js
+++ src/controllers/customer.controllers.js
@@ -19,14 +19,20 @@ export async function postCustomersController(req, res) {
 
 export async function putCustomersController(req, res) {
     const { name, phone, cpf, birthday } = req.body;
-    const { id } = req.params;
+    const id = Number(req.params.id);
     try {
-        const cpfExists = await db.query(`SELECT * FROM customers WHERE cpf = $1`, [cpf]);
-        if (cpfExists.rowCount > 0) {
-            return res.status(409).send('CPF already exists');
+        const customers = await db.query("SELECT * FROM customers WHERE id=$1", [id]);
+        if (customers.rows.length === 0){
+            return res.status(404).send("Nenhum cliente cadastrado com este ID");
         }
-        await db.query(`UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5`, [name, phone, cpf, birthday, id]);
-        res.status(200).send('Customer updated successfully');
+
+        const customerAlreadyRegistered = await db.query("SELECT * FROM customers WHERE cpf=$1  AND id!=$02", [cpf, id]);
+        if (customerAlreadyRegistered.rows.length != 0){
+            return res.status(409).send("O CPF informado já está sendo utilizado em outro cadastro");
+        }
+
+        await db.query("UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5", [name, phone, cpf, birthday, id]);
+        res.sendStatus(200);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
