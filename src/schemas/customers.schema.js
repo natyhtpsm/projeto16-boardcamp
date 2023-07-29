94199d70d11b211de15eba97e63c4d92698d19f9
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-29 15:25:21 -0300

    Update: customer schema

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -1,8 +1,8 @@
 import joi from "joi";
 
 export const customerSchema = joi.object({
-    name: joi.string().required(),
-    phone: joi.number().required(),
-    cpf: joi.required(),
+    name: joi.string().min(2).required(),
+    phone: joi.number().min(2).required(),
+    cpf: joi.required().min(11),
     birthday: joi.required(),
 });
