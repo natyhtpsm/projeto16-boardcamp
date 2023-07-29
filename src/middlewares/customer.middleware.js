d7065f3dd3162fdd2b257fb917f1cc9d5263271b
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-29 18:14:03 -0300

    Fix: name validation customer

--- src/middlewares/customer.middleware.js
+++ src/middlewares/customer.middleware.js
@@ -8,10 +8,15 @@ export function customersMiddleware(req, res, next) {
         return res.status(400).send(validation.error.message);
     }
 
+    if(name.length === 0){
+        return res.status(400).send('Invalid name');
+    }
     if (cpf.length !== 11) {
         return res.status(400).send('Invalid CPF');
     }
-
+    if(phone.length !== 11){
+        return res.status(400).send('Invalid phone number');
+    }
     const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
     if (!dateRegex.test(birthday)) {
         return res.status(400).send('Invalid date format');
