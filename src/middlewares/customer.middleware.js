865384ceebfc44bc3ae9e25b957204a90e18dbd5
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-29 15:30:47 -0300

    Feat: customer middleware

---
+++ src/middlewares/customer.middleware.js
@@ -0,0 +1,21 @@
+import { customerSchema } from "../schemas/customers.schema.js";
+
+export function customersmiddleware(req, res, next) {
+    const { name, phone, cpf, birthday } = req.body;
+    const { error } = customerSchema.validate(req.body);
+    
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    if (cpf.length !== 11) {
+        return res.status(400).send('Invalid CPF');
+    }
+
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateRegex.test(birthday)) {
+        return res.status(400).send('Invalid date format');
+    }
+    next();
+}
+
