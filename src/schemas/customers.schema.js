4dbdc1d69061bb0d914cbc6f6fa52f719f676428
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-29 10:45:46 -0300

    Update: costumer schema

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -1,6 +1,6 @@
 import joi from "joi";
 
-const customerSchema = joi.object({
+export const customerSchema = joi.object({
     name: joi.string().required(),
     phone: joi.number().required(),
     cpf: joi.required(),
