0a8bd51c678b9c13ddb5005a220a9d8a8891d41b
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-29 17:46:09 -0300

    Fix: validation customer

--- src/middlewares/customer.middleware.js
+++ src/middlewares/customer.middleware.js
@@ -2,17 +2,17 @@ import { customerSchema } from "../schemas/customers.schema.js";
 
 export function customersMiddleware(req, res, next) {
     const { name, phone, cpf, birthday } = req.body;
-    const { error } = customerSchema.validate(req.body);
+    const validation = customerSchema.validate(req.body);
     
-    if (error) {
-        return res.status(400).send(error.details[0].message);
+    if (validation.error) { 
+        return res.status(400).send(validation.error.message);
     }
 
     if (cpf.length !== 11) {
         return res.status(400).send('Invalid CPF');
     }
 
-    const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
     if (!dateRegex.test(birthday)) {
         return res.status(400).send('Invalid date format');
     }
